5737c61a08bb425f831300f1035557d7bd80aebc
Baptiste BERTRAND-RAPELLO <baptiste.bertrand-rapello@dedalus.eu>
2022-06-03 19:00:31 +0200

    :construction: juste test de fonction

--- justeUneFonction.sh
+++ justeUneFonction.sh
@@ -5,13 +5,16 @@ print_something ()
     printf "Bonjour depuis la fonction\n"
 }
 
-print_arg (TEST) 
+print_arg () 
 {
-    printf "ceci est test $TEST"
+    printf "il y a $# arguments\n"
+    printf "ceci est test $0 $1 $2\n"
 }
 
 
 print_something
 
-print_arg("ceci")
-print_arg(42)
+print_arg "'argument 0'" "'argument 1'"
+print_arg 42
+
+print_arg 42 36 12 98 8475
